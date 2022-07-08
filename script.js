4c03af82961afce8ed88ee5b51b3ac18b414881b
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-07-08 13:33:31 +0530

    video play/pause functionality

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+const video =document.querySelector('video');
+const progressRange=document.querySelector('.progress-range');
+const progressBar=document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange=document.querySelector('.volume-range');
+const volumeBar=document.querySelector('.volume-bar');
+const currentTime=document.querySelector('time-elapsed');
+const duration=document.querySelector('.time-duration');
+const fullscreenBtn=document.querySelector('.fullscreen');
 
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause','fa-play');
+    playBtn.setAttribute('title','Play');
+}
 
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playBtn.classList.replace('fa-play','fa-pause');
+        playBtn.setAttribute('title','Pause');
+    }else{
+        video.pause();
+       showPlayIcon();
+    }
+}
+// on video end show play button
+video.addEventListener('ended',showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
@@ -19,3 +45,6 @@
 // Fullscreen ------------------------------- //
 
 
+// eventlistener
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
