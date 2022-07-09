194973651baa5977ab4b4b5ca2fc557b52894939
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-07-09 11:23:37 +0530

    added current time duration and progress bar seek functionality

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
-const video =document.querySelector('video');
-const progressRange=document.querySelector('.progress-range');
-const progressBar=document.querySelector('.progress-bar');
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
 const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
-const volumeRange=document.querySelector('.volume-range');
-const volumeBar=document.querySelector('.volume-bar');
-const currentTime=document.querySelector('time-elapsed');
-const duration=document.querySelector('.time-duration');
-const fullscreenBtn=document.querySelector('.fullscreen');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 
 // Play & Pause ----------------------------------- //
 
 function showPlayIcon() {
-    playBtn.classList.replace('fa-pause','fa-play');
-    playBtn.setAttribute('title','Play');
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
 }
 
 function togglePlay() {
     if (video.paused) {
         video.play();
-        playBtn.classList.replace('fa-play','fa-pause');
-        playBtn.setAttribute('title','Pause');
-    }else{
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    } else {
         video.pause();
-       showPlayIcon();
+        showPlayIcon();
     }
 }
 // on video end show play button
-video.addEventListener('ended',showPlayIcon);
+video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
-// update progress bar//
-function updateProgress() {
-progressBar.style.width=`${(video.currentTime/video.duration)*100}%`
+
+// displayTime
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
 }
 
+// update progress bar//
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
+
+
+// click to seek 
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+    
+}
 
 
 // Volume Controls --------------------------- //
@@ -53,5 +72,6 @@ progressBar.style.width=`${(video.currentTime/video.duration)*100}%`
 // eventlistener
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
-video.addEventListener('timeupdate',updateProgress);
-video.addEventListener('canplay',updateProgress);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
